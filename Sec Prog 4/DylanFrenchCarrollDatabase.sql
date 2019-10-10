45f5ee0e7f6b5f32a23a9b40037d517a432f77cc
Dylan French Carroll <Dylan.French.Carroll@gmail.com>
2019-10-10 11:00:17 +0100

    Add files via upload

---
+++ Sec Prog 4/DylanFrenchCarrollDatabase.sql
@@ -0,0 +1,17 @@
+CREATE TABLE people(
+name varchar(20),
+password varchar(20)
+ );
+
+INSERT INTO people
+VALUES("root", "root");
+INSERT INTO people
+VALUES("root1", "root1");
+INSERT INTO people
+VALUES("root2", "root2");
+INSERT INTO people
+VALUES("root3", "root3");
+INSERT INTO people
+VALUES("root4", "root4");
+INSERT INTO people
+VALUES("root5", "root5");
